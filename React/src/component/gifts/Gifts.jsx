7ae7ee1b8d639288e26ef769298695effa916d5c
이정호 <skjongho@naver.com>
2022-06-14 11:26:04 +0900

    branch backup

--- React/src/component/gifts/Gifts.jsx
+++ React/src/component/gifts/Gifts.jsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react";
 import { selectAllgifts } from "../../app/gifts";
+import { IMG_PATH } from "../../http/CustomAxios";
 
 const Gifts=()=>{
-    const gifts=useSelector((state)=>state.gifts.allGifts);
-    console.log(gifts);
+    const allGifts=useSelector((state)=>state.gifts.allGifts);
+    console.log(allGifts.gifts);
     const dispatch=useDispatch();
     const getAllGifts = () => {
         dispatch(selectAllgifts());
@@ -15,19 +16,27 @@ const Gifts=()=>{
 
     return(
         <div className="container">
-            <div className="title">
+            <div className="row text-center">
                 <h1>선물하기</h1>
             </div>
-            <div className="Gifts">
-                {/* {gifts?.map((gift)=>(
+            <div className="row row-cols-4">
+                {allGifts.gifts?.map((gift,index)=>(
+                    <figure key={index} className="figure">
+                         <figcaption className="figure-caption text-center">{gift.name}</figcaption>
                         <img
-                            className="GiftsImg"
+                            className="img-thumbnail col"
                             key={gift.id}
-                            src={gift.img}
+                            src={`${IMG_PATH}${gift.img}`}
                             alt={gift.name}
+                            id={gift.id}
+                            content={gift.content}
+                            count={gift.count}
+                            name={gift.name}
+                            view={gift.views}
                         ></img>
+                    </figure>
                 ))}
-             */}
+            
             </div>
         </div>
         

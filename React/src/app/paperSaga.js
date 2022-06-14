5543aa6b799fd92daa16bd37c20a4a12e4f6d51d
임기안 <dlarldks@naver.com>
2022-06-14 15:01:08 +0900

    전환화면 구현

--- React/src/app/paperSaga.js
+++ React/src/app/paperSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { getPapers, getPapersFail, load, load2 } from './paper';
 import { defaultAxios } from './AxiosApi';
 
@@ -12,26 +12,26 @@ function* postPaper(data) {
         yield put(getPapersFail(error));
         console.error(error);
     }
+}
 
 function* handleGetAllPaper() {
-  try {
-    const paper = yield call(defaultAxios, "/paper", "get", null);
-    yield put(getPapers(paper.data));
-  } catch (error) {
-    console.error(error);
-    yield put(getPapersFail(error));
-  }
-
+    try {
+        const paper = yield call(defaultAxios, '/paper', 'get', null);
+        yield put(getPapers(paper.data));
+    } catch (error) {
+        console.error(error);
+        yield put(getPapersFail(error));
+    }
 }
 function* handleGetPaperById() {
-  try {
-    const id = yield select((state) => state.user.me.uid);
-    const paper = yield call(defaultAxios, `/paper/${id}`, "get", null);
-    yield put(getPapers(paper.data));
-  } catch (error) {
-    console.error(error);
-    yield put(getPapersFail(error));
-  }
+    try {
+        const id = yield select((state) => state.user.me.uid);
+        const paper = yield call(defaultAxios, `/paper/${id}`, 'get', null);
+        yield put(getPapers(paper.data));
+    } catch (error) {
+        console.error(error);
+        yield put(getPapersFail(error));
+    }
 }
 export function* watchGetPaper() {
     yield yield takeLatest(load2, postPaper);
--- React/src/component/profile/ProfileMain.jsx
+++ React/src/component/profile/ProfileMain.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import ProfileBody from "./ProfileBody";
-import AddPaperForm from "./AddPaperForm";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ProfileBody from './ProfileBody';
+import AddPaperForm from './AddPaperForm';
+import SelectUption from './SelectUption';
 
 const ProfileMain = () => {
-  const selectedUser = useSelector((state) => state.paper.selectedUser);
-  const me = useSelector((state) => state.user.me);
-  return <>{selectedUser?.uid === me?.uid ? <ProfileBody /> : <AddPaperForm />}</>;
+    const selectedUser = useSelector((state) => state.paper.selectedUser);
+    const me = useSelector((state) => state.user.me);
+    return <>{selectedUser?.uid === me?.uid ? <ProfileBody /> : <SelectUption />}</>;
 };
 
 export default ProfileMain;

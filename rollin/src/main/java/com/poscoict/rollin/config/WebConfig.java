36646852100481a3ffcfe24f87f6c5f5c5d506a1
임기안 <dlarldks@naver.com>
2022-06-14 14:05:09 +0900

    이것도 보냅니다

--- rollin/src/main/java/com/poscoict/rollin/config/WebConfig.java
+++ rollin/src/main/java/com/poscoict/rollin/config/WebConfig.java
@@ -1,9 +1,11 @@
+package com.poscoict.rollin.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")

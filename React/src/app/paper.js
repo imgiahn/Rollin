f3b2247cab7277c30efdb165cbface37bca17880
임기안 <dlarldks@naver.com>
2022-06-15 11:56:19 +0900

    dd

--- React/src/app/paper.js
+++ React/src/app/paper.js
@@ -1,13 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { Papers } from "../data/Paper";
+import { createSlice } from '@reduxjs/toolkit';
+import { Papers } from '../data/Paper';
 
 // import { defaultAxios } from "./AxiosApi";
 const initialState = {
-  paper: Papers,
-  isLoading: false,
-  error: null,
-  selectedUser: {},
-  selectedPaper: {},
+    paper: Papers,
+    isLoading: false,
+    error: null,
+    selectedUser: {},
+    selectedPaper: {},
 };
 
 const paperSlice = createSlice({
@@ -17,42 +17,46 @@ const paperSlice = createSlice({
         select: (state, action) => {
             state.selectedUser = action.payload;
         },
+        selectPaper: (state, { payload }) => {
+            console.log(payload);
+            state.selectedPaper = payload;
+        },
+        getPapers: (state, { payload }) => {
+            console.log(payload);
+            state.paper = payload;
+        },
+        load: (state) => {
+            state.isLoading = true;
+        },
+        load2: (state, action) => {
+            //   state.paperdata = action.payload;
+            console.log(action.payload);
+        },
+        getPapersFail: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+        requestGetGift: (state, action) => {
+            console.log('requestGetGift');
+        },
+        getGiftById: (state, { payload }) => {
+            state.selectedPaper = { ...state.selectedPaper, gift: payload };
+        },
+        getGiftByIdFails: (state, { payload }) => {
+            state.error = payload;
+        },
     },
-    selectPaper: (state, { payload }) => {
-      console.log(payload);
-      state.selectedPaper = payload;
-    },
-    getPapers: (state, { payload }) => {
-      console.log(payload);
-      state.paper = payload;
-    },
-    load: (state) => {
-      state.isLoading = true;
-    },
-    load2: (state, action) => {
-      //   state.paperdata = action.payload;
-      console.log(action.payload);
-    },
-    getPapersFail: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    requestGetGift: (state, action) => {
-      console.log("requestGetGift");
-    },
-    getGiftById: (state, { payload }) => {
-      state.selectedPaper = { ...state.selectedPaper, gift: payload };
-    },
-    getGiftByIdFails: (state, { payload }) => {
-      state.error = payload;
-    },
-    // getSeletedUserPapers: (state, action) => {
-    //   const response = defaultAxios(`/paper/${action.payload.id}`, "get", null);
-    //   console.log(response);
-    //   state.paper = response;
-    // },
-  },
 });
-export const { select, selectPaper, getPapers, getPapersFail, getSeletedUsersPapers, load, load2, getGiftById, getGiftByIdFails, requestGetGift } =
-  paperSlice.actions;
+export const {
+    select,
+    selectPaper,
+    getPapers,
+    getPapersFail,
+    getSeletedUsersPapers,
+    load,
+    load2,
+    getGiftById,
+    getGiftByIdFails,
+    requestGetGift,
+} = paperSlice.actions;
 export default paperSlice.reducer;

c702866165a8ee15c14fc085eda1041207b3c779
ayeon <gudwodjssl@gmail.com>
2022-06-17 15:41:30 +0900

    feat

--- rollin/src/main/java/com/poscoict/rollin/mail/service/MailServiceImpl.java
+++ rollin/src/main/java/com/poscoict/rollin/mail/service/MailServiceImpl.java
@@ -21,13 +21,13 @@ public class MailServiceImpl implements MailService{
         message.setFrom("gudwodjssl2@gmail.com"); // from 값을 설정하지 않으면 application.yml의 username값이 설정
         message.setSubject(mail.getTitle());
 
-        String msg = "=========================================================";
+        String msg = "=========================================================\n";
 
         msg += mail.getMessage() + "\n\n";
         msg += "상품 이름 : " + mail.getGiftName() + "\n";
         msg += "상품 가격 : " + mail.getGiftPrice() + "원 \n";
         msg += "상품 설명 : " + mail.getGiftContent() + "\n";
-        msg += "환불은 불가능합니다.\n감사합니다";
+        msg += "환불은 불가능합니다.\n감사합니다\n";
         msg += "=========================================================";
 
         message.setText(msg);
